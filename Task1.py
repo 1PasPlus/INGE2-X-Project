114b2830c51b4b6c063d6324184e00dcd491686e
Vecteurr <victor.asencio@esme.fr>
2024-03-17 10:51:35 +0100

    GoogleNews

--- Task1.py
+++ Task1.py
@@ -1,1 +1,13 @@
 print ("Hello World")
+
+
+from GoogleNews import GoogleNews
+
+gnews = GoogleNews()
+
+gnews.set_time_range('01/03/2024','16/03/2024')
+
+gnews.search('bictoin')
+
+print(gnews.results())
+
