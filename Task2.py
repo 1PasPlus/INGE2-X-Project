4ae137a01b3f3e6d4e136d695019ce44a991686e
Vecteurr <victor.asencio@esme.fr>
2024-03-24 13:01:19 +0100

    Gros travail sur les task 2 et 2Bis

--- Task2.py
+++ Task2.py
@@ -25,15 +25,40 @@ choix = input("Voulez-vous des actualités Top du moment (1) ou des actualités 
 articles_info = []
 
 if choix == "1":
-    time_range = str(input("Sur combien de jour souhaiter vous couvrir l'info en tendance?\nEntrez un nombre entier : "))
-    speek = str(input("Dans quelle langue voulez-vous l'info (fr, eng, it,) : "))
-    nb_resultats = int(input("Combien de résultats souhaitez-vous afficher? "))
-    nationality = str(input("De quel pays souhaitez-vous que l'info vienne? Exemple : FR, ENG, USA"))
+    #time_range = str(input("Sur combien de jour souhaiter vous couvrir l'info en tendance?\nEntrez un nombre entier : "))
+    #speek = str(input("Dans quelle langue voulez-vous l'info (fr, eng, it,) : "))
+    #nb_resultats = int(input("Combien de résultats souhaitez-vous afficher? "))
+    #nationality = str(input("De quel pays souhaitez-vous que l'info vienne? Exemple : FR, ENG, USA"))
     #time_range_1 = input("Entrez au format JJ/MM/AAAA la timeframe que vous souhaitez appliquer à votre recherche\nATTENTION ! Tous les résultats seront issus d'articles parus dans cet intervale de temps\nDate de début")
     #time_range_2 = input("Date de fin (au format JJ/MM/AAAA)")
-    client = GNews(language=speek, country=nationality, period=time_range+'d', start_date=None, end_date=None, max_results=nb_resultats)
+    #client = GNews(language=speek, country=nationality, period=time_range+'d', start_date=None, end_date=None, max_results=nb_resultats)
+    
+    client = GNews(language="fr", country="FR", period='7d', start_date=None, end_date=None, max_results=10)
     articles = client.get_top_news()
 
+    df = pd.DataFrame({
+        "Titre": [article['title'].split(' - ')[0] for article in articles],
+        "Source": [article['title'].split(' - ')[1] for article in articles],
+        #"source2" : [article['publisher'] for article in articles], mauvais format "{'href': 'https://www.20minutes.fr'
+        "url" : [article['url'] for article in articles],
+        "published date": [article['published date']for article in articles],
+        "description": [article['description']for article in articles]
+    })  
+
+    # Récupération des liens de chaque ligne du DataFrame
+    for index, row in df.iterrows():
+        link = row['url']
+        meta, contenu = get_more_info(link)
+        df = df.append({
+            'Meta description': meta,
+            'Contenu': contenu 
+            }, ignore_index=True
+        )
+    
+    df.head(5)
+    df.info()
+    df.to_csv("choix_tendance.csv", sep=";", index=False)
+    
 elif choix == "2":
     keyword = input("\n\nEntrez votre mot-clé : ")
     start = input("\n\nEntrez au format JJ/MM/AAAA la timeframe que vous souhaitez appliquer à votre recherche\nATTENTION ! Tous les résultats seront issus d'articles parus dans cet intervale de temps\nDate de début : ")
@@ -72,9 +97,11 @@ elif choix == "2":
     #Petit dataframe des familles tu connais 
     df = pd.DataFrame(articles_info)
     #print(df.info())
-    print(df)
+    #print(df)
     #print(df['Link'])
-    print(df.info())
+    #print(df.info())
+    #print(df['Résumé'])
+    #df.to_csv("choix_sujet.csv", sep=";", index=False)
 
 
 else:
