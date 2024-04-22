a29215c9f21c71a06c0a02a77e5634fa09f9ed2e
N-19-dev <127797502+N-19-dev@users.noreply.github.com>
2024-04-22 12:20:29 +0200

    add files news_scraper.py new version more efficient and it combine de summary text

--- news_scraper.py
+++ news_scraper.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import csv
+from transformers import pipeline
+import datetime
 from GoogleNews import GoogleNews
 from newspaper import Article
 import requests
@@ -6,7 +9,6 @@ from gnews import GNews
 
 #La librairie GoogleNews ne nous donne pas assez d'infos à mon goût donc j'utilise une autre méthode pour gratter qlq infos de plus (voir fonction suivante)
 
-#On prendra ici des infos en plus qui se retrouveront plus tard dans notre dataframe 
 def get_more_info(url):
     response = requests.get(url)
     article = Article(url)
@@ -15,34 +17,32 @@ def get_more_info(url):
 
     meta = article.meta_description
     contenu = article.text
-    #print(contenu)
 
     return meta, contenu
 
-#Fonction qui nous donne les tops articles sur le sujet choisit par l'utilisateur
-def topic_news(keyword,start,end):
+def topic_news(keyword,language,period):
 
     getnews = GoogleNews()
-    getnews.set_time_range(start,end)
+    getnews.set_lang(language)
+    getnews.set_encode('utf-8')
+    getnews.set_period(period)
+
+    #getnews = GoogleNews(language,period,encore="utf-8")
+
     getnews.search(keyword)
     results = getnews.results()
 
     articles_info = []
-    #On fait une boucle pour tous les résultats trouvés par GoogleNews
+
     for result in results:
-
-        # Les url renvoyés par GoogleNews ne sont pas valables, avec un peu de recherche je me suis apperçu qui fallait les slipts avant le premier & dans l'url
-        # Exemple 
-        # Url renoyé par GoogleNews : https://cryptopotato.com/this-happened-on-coinbases-bitcoin-premium-index-before-btc-plunged-to-66k/&ved=2ahUKEwj_to_CyPuEAxWiVqQEHY8kD1kQxfQBegQIABAC&usg=AOvVaw1tRkOHCP-kHNJq5LaGn-7D/
-        # Url valide : https://cryptopotato.com/this-happened-on-coinbases-bitcoin-premium-index-before-btc-plunged-to-66k/
-
-        
+        #Les url renvoyés par GoogleNews ne sont pas valables, avec un peu de recherche je me suis apperçu qui fallait les slipts avant le premier & dans l'url
+        #Exemple 
+        #Url renoyé par GoogleNews : https://cryptopotato.com/this-happened-on-coinbases-bitcoin-premium-index-before-btc-plunged-to-66k/&ved=2ahUKEwj_to_CyPuEAxWiVqQEHY8kD1kQxfQBegQIABAC&usg=AOvVaw1tRkOHCP-kHNJq5LaGn-7D/
+        #Url valide : https://cryptopotato.com/this-happened-on-coinbases-bitcoin-premium-index-before-btc-plunged-to-66k/
         url = result['link'].split('&')[0]
         art = Article(url)
-        #print(url)
 
         try:
-            #On ajoute tts les infos dans un dico pour le dataframe final
             meta, contenu = get_more_info(url)
             article_info = {
                 'Titre': result['title'],
@@ -60,26 +60,10 @@ def topic_news(keyword,start,end):
         except Exception as e:
             print("Erreur lors de la récupération des informations de l'article:", e)
 
-    #Petit dataframe des familles tu connais 
     df = pd.DataFrame(articles_info)
-    #print(df.info())
-    #print(df)
-    #print(df['Link'])
-    #print(df.info())
-    #print(df['Résumé'])
-    #df.to_csv("choix_sujet.csv", sep=";", index=False)
-
     return df
 
-#Fonction qui donne les tops articles en tendance, peu importe le sujet
 def top_news(article_language, article_country, time_period, article_number):
-    #time_range = str(input("Sur combien de jour souhaiter vous couvrir l'info en tendance?\nEntrez un nombre entier : "))
-    #speek = str(input("Dans quelle langue voulez-vous l'info (fr, eng, it,) : "))
-    #nb_resultats = int(input("Combien de résultats souhaitez-vous afficher? "))
-    #nationality = str(input("De quel pays souhaitez-vous que l'info vienne? Exemple : FR, ENG, USA"))
-    #time_range_1 = input("Entrez au format JJ/MM/AAAA la timeframe que vous souhaitez appliquer à votre recherche\nATTENTION ! Tous les résultats seront issus d'articles parus dans cet intervale de temps\nDate de début")
-    #time_range_2 = input("Date de fin (au format JJ/MM/AAAA)")
-    #client = GNews(language=speek, country=nationality, period=time_range+'d', start_date=None, end_date=None, max_results=nb_resultats)
     
     client = GNews(language=article_language, country=article_country, period=time_period, start_date=None, end_date=None, max_results=article_number)
     articles = client.get_top_news()
@@ -87,13 +71,11 @@ def top_news(article_language, article_country, time_period, article_number):
     df = pd.DataFrame({
         "Titre": [article['title'].split(' - ')[0] for article in articles],
         "Source": [article['title'].split(' - ')[1] for article in articles],
-        #"source2" : [article['publisher'] for article in articles], mauvais format "{'href': 'https://www.20minutes.fr'
         "url" : [article['url'] for article in articles],
         "Date": [article['published date']for article in articles],
         "Description": [article['description']for article in articles]
     })  
 
-     # Récupération des liens de chaque ligne du DataFrame
     for index, row in df.iterrows():
         link = row['url']
         meta, contenu = get_more_info(link)
@@ -101,6 +83,50 @@ def top_news(article_language, article_country, time_period, article_number):
         df.at[index, 'Contenu'] = contenu
 
     return df
+
+def split_into_segments(text, max_segment_length=1000):
+    segments = []
+    current_segment = ""
+    words = text.split()
+    for word in words:
+        if len(current_segment) + len(word) < max_segment_length:
+            current_segment += word + " "
+        else:
+            segments.append(current_segment.strip())
+            current_segment = word + " "
+    segments.append(current_segment.strip())
+    return segments
+
+def summarize_w_bart (df):
+
+    articles = df["Contenu"]
+    articles_sum_bart = []
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    
+    count = 0
+    
+    for article in articles:
+        summaries = []  # Réinitialiser la liste des résumés pour chaque nouvel article
+        
+        if type(article) != str:
+            pass
+        
+        else:
+            segments = split_into_segments(article)
+            
+            for segment in segments:
+                summary = summarizer(segment, max_length=1000, min_length=30, do_sample=False)
+                summaries.append(summary[0]['summary_text'])
+            
+    
+            articles_sum_bart.append(" ".join(summaries))
+            count += 1
+            print("----------- article numéro", count,"completed. All informations are stored -----------")
+    
+
+    X = pd.DataFrame({'Résumé Bart': articles_sum_bart})
+
+    return X
 
 choix = input("Voulez-vous des actualités Top du moment (1) ou des actualités en rapport avec un mot-clé (2) ? ")
 
@@ -156,21 +182,41 @@ if choix == "1":
     time_range = range + 'd'
     nb_results = int(input("\n\nCombien de résultats souhaitez-vous afficher? "))
 
-    # Appel de la fonction top_news avec les paramètres validés
     df = top_news(speak, nationality, time_range, nb_results)
-    #print(df.info())
-    #print(df)
     df.to_csv("choix_tendances.csv", sep=";", index=False)
 
+    # Lecture de chaque ligne du fichier CSV et traitement individuel
+    with open('choix_tendances.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        data = []
+        for row in reader:
+            data.append(row)
+
+    info = pd.DataFrame(data)
+    X = summarize_w_bart(info)
+    final_df = pd.concat([info, X], axis=1)
+    final_df.to_csv("article_tendance_sum.csv", sep=";", index=False, quoting=csv.QUOTE_ALL)
+    
 elif choix == "2":
-    # Code pour le deuxième choix
     keyword = input("\n\nEntrez votre mot-clé : ")
-    start = input("\n\nEntrez au format JJ/MM/AAAA la timeframe que vous souhaitez appliquer à votre recherche\nATTENTION ! Tous les résultats seront issus d'articles parus dans cet intervale de temps\nDate de début : ")
-    end = input("\n\nDate de fin (au format JJ/MM/AAAA) : ")
-    df = topic_news(keyword,start,end)
-    print(df)
-    print(df.info())
+    range = str(input("\n\nSur combien de jour souhaiter vous couvrir l'info sur votre sujet?\nEntrez un nombre entier : "))
+    time_range = range + 'd'
+    speak = input("Dans quelle langue voulez-vous l'info (fr, en, it, etc) : ")
+
+    df = topic_news(keyword,speak,time_range)
     df.to_csv("choix_topic.csv", sep=";", index=False)
+
+    # Lecture de chaque ligne du fichier CSV et traitement individuel
+    with open('choix_topic.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        data = []
+        for row in reader:
+            data.append(row)
+
+    info = pd.DataFrame(data)
+    X = summarize_w_bart(info)
+    final_df = pd.concat([info, X], axis=1)
+    final_df.to_csv("article_topic_sum.csv", sep=";", index=False, quoting=csv.QUOTE_ALL)
 
 else:
     print("Choix invalide.")
